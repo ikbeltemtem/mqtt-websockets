ee6fb6bf4a0bfea956e5cc817062ccaeb70f1822
ikbeltemtem <ikbel.temtem@ensi-uma.tn>
2024-01-16 20:19:22 +0100

    final

--- publisher/publisher.js
+++ publisher/publisher.js
@@ -1,25 +1,32 @@
+// MQTT client, publishing status flag, and interval ID variables
 let mqttClient;
 let isPublishing = false; // Flag to track publishing status
 let publishIntervalId; // Interval ID for publishing
 
+// Event listener for when the window is fully loaded
 window.addEventListener("load", (event) => {
+  // Establish MQTT connection when the window is loaded
   connectToBroker();
 
+  // Event listener for the "Start" button
   const startBtn = document.querySelector(".start");
   startBtn.addEventListener("click", function () {
     startPublishing();
   });
 
+  // Event listener for the "Stop" button
   const stopBtn = document.querySelector(".stop");
   stopBtn.addEventListener("click", function () {
     stopPublishing();
   });
 });
 
+// Function to connect to the MQTT broker
 function connectToBroker() {
+  // Generate a random client ID
   const clientId = "client" + Math.random().toString(36).substring(7);
 
-  // Change this to point to your MQTT broker
+  // MQTT broker connection parameters
   const host = "ws://127.0.0.1:9001/mqtt";
   const options = {
     keepalive: 60,
@@ -31,8 +38,10 @@ function connectToBroker() {
     connectTimeout: 30 * 1000,
   };
 
+  // Connect to the MQTT broker
   mqttClient = mqtt.connect(host, options);
 
+  // Event handlers for MQTT client
   mqttClient.on("error", (err) => {
     console.log("Error: ", err);
     mqttClient.end();
@@ -46,32 +55,41 @@ function connectToBroker() {
     console.log("Client connected:" + clientId);
   });
 
-  // Received
+  // Event handler for received MQTT messages
   mqttClient.on("message", (topic, message, packet) => {
     console.log(
       "Received Message: " + message.toString() + "\nOn topic: " + topic
     );
+    // Display the received temperature in the UI
     displayTemperature(message.toString());
   });
 }
 
+// Function to start publishing random temperatures
 function startPublishing() {
   if (!isPublishing) {
+    // Set publishing flag to true
     isPublishing = true;
-    publishTemperature(); // Publish immediately
+    // Publish temperature immediately
+    publishTemperature();
     // Publish a random temperature every 5 seconds
     publishIntervalId = setInterval(publishTemperature, 5000);
   }
 }
 
+// Function to stop publishing temperatures
 function stopPublishing() {
   if (isPublishing) {
+    // Set publishing flag to false
     isPublishing = false;
+    // Clear the publishing interval
     clearInterval(publishIntervalId);
   }
 }
 
+// Function to publish a random temperature to the MQTT broker
 function publishTemperature() {
+  // MQTT topic for temperature
   const topic = "Temp";
 
   // Function to generate a random temperature between 20 and 30 degrees Celsius
@@ -79,7 +97,7 @@ function publishTemperature() {
     return (Math.random() * 10 + 20).toFixed(2);
   }
 
-  // Publish the generated temperature value to the specified topic
+  // Publish the generated temperature to the specified topic
   const temperature = generateRandomTemperature();
   console.log(`Sending Topic: ${topic}, Temperature: ${temperature}`);
 
@@ -89,7 +107,10 @@ function publishTemperature() {
   });
 }
 
+// Function to display the received temperature in the UI
 function displayTemperature(temperature) {
+  // Find the HTML element with the ID "temperature-display"
   const temperatureDisplay = document.querySelector("#temperature-display");
+  // Update the content of the element to display the received temperature
   temperatureDisplay.textContent = `Received Temperature: ${temperature} °C`;
 }
--- subscriber/subscriber.js
+++ subscriber/subscriber.js
@@ -1,26 +1,33 @@
+// MQTT client variable
 let mqttClient;
 
-// Seuil de température pour déclencher l'alerte
+// Temperature threshold to trigger the alert
 const seuilTemperature = 25;
 
+// Event listener for when the window is fully loaded
 window.addEventListener("load", (event) => {
+  // Establish MQTT connection when the window is loaded
   connectToBroker();
 
+  // Event listener for the "Subscribe" button
   const subscribeBtn = document.querySelector("#subscribe");
   subscribeBtn.addEventListener("click", function () {
     subscribeToTopic();
   });
 
+  // Event listener for the "Unsubscribe" button
   const unsubscribeBtn = document.querySelector("#unsubscribe");
   unsubscribeBtn.addEventListener("click", function () {
     unsubscribeToTopic();
   });
 });
 
+// Function to connect to the MQTT broker
 function connectToBroker() {
+  // Generate a random client ID
   const clientId = "client" + Math.random().toString(36).substring(7);
 
-  // Change this to point to your MQTT broker
+  // MQTT broker connection parameters
   const host = "ws://127.0.0.1:9001/mqtt";
   const options = {
     keepalive: 60,
@@ -32,8 +39,10 @@ function connectToBroker() {
     connectTimeout: 30 * 1000,
   };
 
+  // Connect to the MQTT broker
   mqttClient = mqtt.connect(host, options);
 
+  // Event handlers for MQTT client
   mqttClient.on("error", (err) => {
     console.log("Error: ", err);
     mqttClient.end();
@@ -47,41 +56,53 @@ function connectToBroker() {
     console.log("Client connected:" + clientId);
   });
 
-  // Received
+  // Event handler for received MQTT messages
   mqttClient.on("message", (topic, message, packet) => {
     console.log(
       "Received Message: " + message.toString() + "\nOn topic: " + topic
     );
+    // Display the received temperature in the UI and check for the threshold
     displayTemperature(message.toString());
   });
 }
 
+// Function to subscribe to an MQTT topic
 function subscribeToTopic() {
   const status = document.querySelector("#status");
+  // MQTT topic to subscribe to
   const topic = "Temp";
   console.log(`Subscribing to Topic: ${topic}`);
 
+  // Subscribe to the specified topic 
   mqttClient.subscribe(topic, { qos: 0 });
+  // Update status display to show subscription status
   status.style.color = "green";
   status.textContent = `SUBSCRIBED to ${topic}`;
 }
 
+// Function to unsubscribe from an MQTT topic
 function unsubscribeToTopic() {
   const status = document.querySelector("#status");
+  // MQTT topic to unsubscribe from
   const topic = "Temp";
   console.log(`Unsubscribing from Topic: ${topic}`);
 
+  // Unsubscribe from the specified topic
   mqttClient.unsubscribe(topic, { qos: 0 });
+  // Update status display to show unsubscription status
   status.style.color = "red";
   status.textContent = `UNSUBSCRIBED from ${topic}`;
 }
 
+// Function to display the received temperature in the UI
 function displayTemperature(temperature) {
   const temperatureDisplay = document.querySelector("#temperature-display");
+  // Update the content of the element to display the received temperature
   temperatureDisplay.textContent = `Received Temperature: ${temperature} °C`;
 
-  // Vérifier si la température dépasse le seuil
+  // Check if the temperature exceeds the threshold
   if (parseFloat(temperature) > seuilTemperature) {
-    alert("Alerte : La température a dépassé le seuil !");
+    // Display an alert if the temperature surpasses the threshold
+    alert("Alert: Temperature has exceeded the threshold!");
   }
 }
